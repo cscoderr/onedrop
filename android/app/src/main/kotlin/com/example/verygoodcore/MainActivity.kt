e9682d3aac66a880a8eb3b49a68f2b03a1fe5729
Tomiwa Idowu <tomiwaidowuu@gmail.com>
2023-09-20 22:36:50 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.onedrop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:onedrop/app/app.dart';
+import 'package:onedrop/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
